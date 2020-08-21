9faff4cafc06554ad9089b6e5ca7bf646c786a86
Nopparut Suwannaboon <nopparut.bit@hotmail.com>
2020-08-22 00:58:08 +0700

    fix box scrolling

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,19 +1,50 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  AnotherUser: {
+    borderRadius: "12px",
+    border: "2px solid green",
+    height: "70%",
+    marginLeft: "15px",
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: "10px",
+    paddingRight: "10px",
+  },
+  Me: {
+    borderRadius: "12px",
+    border: "2px solid green",
+    height: "70%",
+    marginRight: "15px",
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: "10px",
+    paddingRight: "10px",
+  },
+  wrapBox: {
+    display: "flex",
+    alignItems: "center",
+    height: "50px",
+  },
+}));
 
 const Message = ({ message: { text, user }, name }) => {
+  const classes = useStyles();
   let isCurrentUser = false;
   const trimname = name.trim();
   if (user === trimname) {
     isCurrentUser = true;
   }
   return isCurrentUser ? (
-    <Box display="flex" justifyContent="flex-end" alignItems="center">
-      <Box borderRadius="12px">{text}</Box>
+    <Box className={classes.wrapBox} justifyContent="flex-end">
+      <Box className={classes.Me} >
+        {text}
+      </Box>
     </Box>
   ) : (
-    <Box display="flex" justifyContent="flex-start" alignItems="center">
-      <Box borderRadius="12px">{text}</Box>
+    <Box className={classes.wrapBox} justifyContent="flex-start">
+      <div className={classes.AnotherUser}>{text}</div>
     </Box>
   );
 };
--- frontend/src/components/Window.js
+++ frontend/src/components/Window.js
@@ -1,22 +1,20 @@
-import React,{useEffect} from 'react'
-import { Box } from '@material-ui/core'
-import ScrollToBottom from 'react-scroll-to-bottom'
-import Message from './Message'
- const Window = ({messages,name}) => {
-     useEffect(() => {
-       console.log(name);
-     }, [name])
-    return (
-        
-        <>
-        <Box width="100%" border="2px solid yellow" height="100%">
-            <ScrollToBottom>
-                {messages.map((message,i) => 
-                    <div key={i}><Message message={message} name={name}/></div>
-                )}
-            </ScrollToBottom>
-        </Box>
+import React from "react";
+import { Box } from "@material-ui/core";
+import ScrollToBottom from "react-scroll-to-bottom";
+import Message from "./Message";
+const Window = ({ messages, name }) => {
+  return (
+    <>
+      <Box width="100%" border="2px solid yellow" height="100%">
+        <ScrollToBottom style={{ overflow: "auto" }}>
+          {messages.map((message, i) => (
+            <div key={i}>
+              <Message message={message} name={name} />
+            </div>
+          ))}
+        </ScrollToBottom>
+      </Box>
     </>
-    )
-}
+  );
+};
 export default Window;
